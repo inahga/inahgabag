7b32cd68dbc80c4f0e4314977ce0887c17b7740d
Ameer Ghani <inahga@gmail.com>
2020-12-13 00:02:17 -0600

    feat: add script for easily patching rpms

---
+++ patch-rpm.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ -z "${1+x}" ] || [ -z "${2+x}" ]; then
+	echo "usage: $0 [TARGET PACKAGE] [PATCH FILES]..."
+	exit 1
+fi
+
+TARGET=$1
+shift 1
+
+install_deps() {
+	dnf install -y make rpm-build dnf-plugins-core
+}
+
+install_srpm() {
+	dnf download --source "$TARGET" --destdir /tmp
+	rpm -ivh "/tmp/$TARGET*"
+}
+
+cp_patches() {
+	for PATCH in "$@"; do
+		cp "$PATCH" "$HOME/rpmbuild/SOURCES/"
+	done
+}
+
+fix_spec() {
+	SPECFILE="$HOME/rpmbuild/SPECS/$TARGET.spec"
+	LATEST_PATCH=$(grep -o 'Patch[0-9]\+:' "$SPECFILE" | tail -n1 || true)
+	if [ -z "$LATEST_PATCH" ]; then
+		PATCHNUMBER=0
+	else
+		PATCHNUMBER=$(($(echo "$LATEST_PATCH" | grep -o '[0-9]\+') + 1))
+	fi
+
+	for PATCH in "$@"; do
+		FULLPATCH="Patch$PATCHNUMBER: $PATCH"
+		if [ -z "$LATEST_PATCH" ]; then
+			sed -i "/^Source0:.*/a $FULLPATCH" "$SPECFILE"
+		else
+			sed -i "/^$LATEST_PATCH.*/a $FULLPATCH" "$SPECFILE"
+		fi
+
+		PATCHNUMBER=$((PATCHNUMBER + 1))
+		LATEST_PATCH=$FULLPATCH
+	done
+}
+
+build() {
+	dnf builddep "$TARGET" -y
+	rpmbuild -ba "$HOME/rpmbuild/SPECS/$TARGET.spec"
+}
+
+install_deps
+install_srpm
+cp_patches "$@"
+fix_spec "$@"
+build
