2a33c90491b3d0c0a25e15a1a607c4cf9fa84004
Ameer Ghani <inahga@gmail.com>
2022-12-04 11:57:56 -0500

    aoc: day 4: memory constant-ish
    Memory usage now proportional to the length of the longest single line.

--- aoc/aoc-2022/src/bin/4.rs
+++ aoc/aoc-2022/src/bin/4.rs
@@ -2,18 +2,25 @@ use std::error::Error;
 use std::io::stdin;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut line = String::new();
     let mut part1 = 0;
     let mut part2 = 0;
 
-    for line in stdin().lines() {
-        let l = line?;
+    while stdin().read_line(&mut line)? != 0 {
+        // Remove trailing newline.
+        line.pop();
 
         // We'll just assume the input is well formatted.
-        let groups = l
+        let mut groups = line
             .split(|c| c == ',' || c == '-')
-            .map(|s| s.parse::<u64>().unwrap())
-            .collect::<Vec<_>>();
-        let (lfirst, rfirst, lsecond, rsecond) = (groups[0], groups[1], groups[2], groups[3]);
+            .map(|s| s.parse::<u64>().unwrap());
+
+        let (lfirst, rfirst, lsecond, rsecond) = (
+            groups.next().unwrap(),
+            groups.next().unwrap(),
+            groups.next().unwrap(),
+            groups.next().unwrap(),
+        );
 
         // Pairs that are subsets.
         if (lfirst <= lsecond && rfirst >= rsecond) || (lfirst >= lsecond && rfirst <= rsecond) {
@@ -24,6 +31,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         if rfirst >= lsecond && lfirst <= rsecond {
             part2 += 1;
         }
+
+        line.clear();
     }
 
     println!("part1: {}", part1);
