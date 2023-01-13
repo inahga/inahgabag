b1d2f01db550ae4bdcdc6fb6e6286f315c2d518e
Ameer Ghani <inahga@gmail.com>
2023-01-12 22:30:46 -0500

    aoc: day 12 kinda broken

--- aoc/aoc-2022/src/bin/12.rs
+++ aoc/aoc-2022/src/bin/12.rs
@@ -1,4 +1,4 @@
-use std::cell::Cell;
+use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::error::Error;
 use std::io::stdin;
@@ -9,9 +9,10 @@ type Point = (usize, usize);
 struct Plot {
     value: char,
     last: Option<Point>,
+    visited: bool,
 }
 
-type Grid = Vec<Vec<Cell<Plot>>>;
+type Grid = Vec<Vec<RefCell<Plot>>>;
 
 fn parse() -> Result<(Grid, Point), Box<dyn Error>> {
     let mut ret = vec![];
@@ -24,9 +25,10 @@ fn parse() -> Result<(Grid, Point), Box<dyn Error>> {
 
         let mut vec_line = vec![];
         for (j, c) in line.chars().enumerate() {
-            vec_line.push(Cell::new(Plot {
+            vec_line.push(RefCell::new(Plot {
                 value: c,
                 last: None,
+                visited: false,
             }));
             if c == 'S' {
                 start = (i, j);
@@ -40,7 +42,7 @@ fn parse() -> Result<(Grid, Point), Box<dyn Error>> {
     Ok((ret, start))
 }
 
-fn candidates(p: Point, dim: usize) -> impl Iterator<Item = Point> {
+fn candidates(p: Point) -> impl Iterator<Item = Point> {
     let mut ret = vec![];
     ret.push((p.0 + 1, p.1));
     ret.push((p.0, p.1 + 1));
@@ -54,38 +56,55 @@ fn candidates(p: Point, dim: usize) -> impl Iterator<Item = Point> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (mut grid, start) = parse()?;
+    let (grid, start) = parse()?;
     let mut found = None;
 
     let mut queue = VecDeque::new();
     queue.push_back(start);
+    {
+        let mut start_plot = grid[start.0][start.1].borrow_mut();
+        start_plot.visited = true;
+    }
     while !queue.is_empty() {
         let curr_pt = queue.pop_front().unwrap();
-        let curr_plot = grid[curr_pt.0][curr_pt.1].get();
+        let curr_plot = grid[curr_pt.0][curr_pt.1].borrow();
 
         for candidate in candidates(curr_pt) {
-            let candidate_plot = grid[candidate.0][candidate.1].get_mut();
-            candidate_plot.last = Some(curr_pt);
+            if candidate.0 >= grid.len() || candidate.1 >= grid[0].len() {
+                continue;
+            }
 
-            if candidate_plot.value == 'E' {
+            let mut candidate_plot = grid[candidate.0][candidate.1].borrow_mut();
+
+            if candidate_plot.value == 'E' && (curr_plot.value == 'z' || curr_plot.value == 'y') {
+                candidate_plot.last = Some(curr_pt);
+                candidate_plot.visited = true;
                 found = Some(candidate_plot.clone());
                 break;
             }
 
             let diff = if curr_plot.value == 'S' {
-                candidate_plot.value as i16 - ('a' as i16 - 1)
+                candidate_plot.value as i16 - ('a' as i16)
             } else {
                 candidate_plot.value as i16 - curr_plot.value as i16
             };
-            if diff == 1 || diff == 0 {
+            if !candidate_plot.visited && diff <= 1 {
+                candidate_plot.last = Some(curr_pt);
+                candidate_plot.visited = true;
                 queue.push_back(candidate);
             }
         }
     }
 
-    // start at E, backtrack last until we find s
-    println!("{:?}", grid);
-    println!("{:?}", found);
+    let mut part1 = 0;
+    let mut iter = found.unwrap();
+    while let Some(i) = iter.last {
+        println!("{:?}", iter);
+        part1 += 1;
+        iter = grid[i.0][i.1].borrow().clone();
+    }
+
+    println!("{:?}", part1);
 
     Ok(())
 }
